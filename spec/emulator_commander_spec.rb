1dc13f4c73c80f4fdd62493c6fa170f88a3a993c
Serghei Moret <serghei.moret@xing.com>
2018-08-26 16:59:51 +0200

    fix typo

--- spec/emulator_commander_spec.rb
+++ spec/emulator_commander_spec.rb
@@ -42,7 +42,7 @@ describe Fastlane::Helper::EmulatorCommander do
       }.to raise_exception(RuntimeError, "Something went wrong. Newly created device couldn't connect to the adb")
     end
 
-    it 'prints a success message if simulator is unhealthy' do
+    it 'prints a success message if simulator is healthy' do
       expect(Fastlane::Actions).to receive(:sh).with("docker exec -i some_name bash -l -c \"adb devices\"").and_return("emulator_5554 \tdevice")
       expect(Fastlane::UI).to receive(:success).with('Checking if emulator is connected to ADB.')
       expect(Fastlane::UI).to receive(:success).with('Emulator connected successfully')
