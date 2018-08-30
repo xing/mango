bd6c17ccb2a68310d050b3d82ff5dee62b6f18e1
Serghei Moret <serghei.moret@xing.com>
2018-08-30 16:53:03 +0200

    Fix shell not being ready to receive commands

--- lib/fastlane/plugin/mango/helper/emulator_commander.rb
+++ lib/fastlane/plugin/mango/helper/emulator_commander.rb
@@ -17,6 +17,14 @@ module Fastlane
         @docker_commander.exec(command: 'adb shell settings put global window_animation_scale 0.0')
         @docker_commander.exec(command: 'adb shell settings put global transition_animation_scale 0.0')
         @docker_commander.exec(command: 'adb shell settings put global animator_duration_scale 0.0')
+      rescue FastlaneCore::Interface::FastlaneShellError
+        # Under weird circumstances it can happen that adb is running but shell on the emu is not completely up
+        # it recovers after some time, so wait and retry
+        retry_counter = retry_counter.to_i + 1
+        if retry_counter <= 5
+          sleep 10*retry_counter
+          retry
+        end
       end
 
       # Increases logcat storage
