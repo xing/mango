756df91140e1cd5fa51d87164e198c193a07e9ff
Serghei Moret <joesatriani323@gmail.com>
2021-08-03 19:07:59 +0200

    proper filename

--- lib/fastlane/plugin/mango/helper/emulator_language.rb
+++ lib/fastlane/plugin/mango/helper/emulator_language.rb
@@ -6,7 +6,7 @@ module Fastlane
 
         language = lang.split('_')[0]
         country = lang.split('_')[1]
-        docker_commander.exec(command: 'adb install ../settings_apk/settings_apk-debug.apk')
+        docker_commander.exec(command: "adb install #{File.join(File.dirname(__FILE__), '..', 'settings_apk', 'settings_apk-debug.apk')}")
         docker_commander.exec(command: 'adb shell pm grant io.appium.settings android.permission.CHANGE_CONFIGURATION')
         docker_commander.exec(command: "adb shell am broadcast -a io.appium.settings.locale -n io.appium.settings/.receivers.LocaleSettingReceiver --es lang #{language} --es country #{country}")
       end
--- lib/fastlane/plugin/mango/version.rb
+++ lib/fastlane/plugin/mango/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module Mango
-    VERSION = '1.3.29'.freeze
+    VERSION = '1.3.30'.freeze
   end
 end
