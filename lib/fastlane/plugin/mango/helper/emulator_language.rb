62312b68e84aae8bce642c813631967fdc595f30
Serghei Moret <joesatriani323@gmail.com>
2021-08-03 21:43:36 +0200

    add a timeout

--- lib/fastlane/plugin/mango/helper/emulator_language.rb
+++ lib/fastlane/plugin/mango/helper/emulator_language.rb
@@ -1,3 +1,5 @@
+require 'timeout'
+
 module Fastlane
   module Helper
     module EmulatorLanguage
@@ -9,9 +11,12 @@ module Fastlane
         country = lang.split('_')[1]
         apk_path = File.join(File.dirname(__FILE__), 'settings.apk')
         docker_commander.cp(file: apk_path)
-        docker_commander.exec(command: 'adb install /root/tests/settings.apk')
-        docker_commander.exec(command: 'adb shell pm grant io.appium.settings android.permission.CHANGE_CONFIGURATION')
-        docker_commander.exec(command: "adb shell am broadcast -a io.appium.settings.locale -n io.appium.settings/.receivers.LocaleSettingReceiver --es lang #{language} --es country #{country}")
+
+        Timeout.timeout(20) do
+          docker_commander.exec(command: 'adb install /root/tests/settings.apk')
+          docker_commander.exec(command: 'adb shell pm grant io.appium.settings android.permission.CHANGE_CONFIGURATION')
+          docker_commander.exec(command: "adb shell am broadcast -a io.appium.settings.locale -n io.appium.settings/.receivers.LocaleSettingReceiver --es lang #{language} --es country #{country}")
+        end
       rescue StandardError => e
         raise e if retries.zero?
 
