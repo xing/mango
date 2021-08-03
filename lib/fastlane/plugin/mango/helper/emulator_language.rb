b1f61e3665d5820c580536596d0f98127a3dae55
Serghei Moret <joesatriani323@gmail.com>
2021-08-03 21:23:30 +0200

    add retries

--- lib/fastlane/plugin/mango/helper/emulator_language.rb
+++ lib/fastlane/plugin/mango/helper/emulator_language.rb
@@ -4,6 +4,7 @@ module Fastlane
       def self.set(lang, docker_commander)
         UI.important("Changing device locale to #{lang}")
 
+        retries ||= 5
         language = lang.split('_')[0]
         country = lang.split('_')[1]
         apk_path = File.join(File.dirname(__FILE__), 'settings.apk')
@@ -11,6 +12,13 @@ module Fastlane
         docker_commander.exec(command: 'adb install /root/tests/settings.apk')
         docker_commander.exec(command: 'adb shell pm grant io.appium.settings android.permission.CHANGE_CONFIGURATION')
         docker_commander.exec(command: "adb shell am broadcast -a io.appium.settings.locale -n io.appium.settings/.receivers.LocaleSettingReceiver --es lang #{language} --es country #{country}")
+      rescue StandardError => e
+        raise e if retries.zero?
+
+        puts "Got an exception: #{e.message}. Will retry in 5 seconds"
+        retries -= 1
+        sleep 5
+        retry
       end
     end
   end
--- lib/fastlane/plugin/mango/version.rb
+++ lib/fastlane/plugin/mango/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module Mango
-    VERSION = '1.3.33'.freeze
+    VERSION = '1.3.34'.freeze
   end
 end
