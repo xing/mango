12bbe5354511e374b3bb257dc46db10b555f38ec
Serghei Moret <joesatriani323@gmail.com>
2021-08-03 17:59:01 +0200

    Update emulator_language.rb

--- lib/fastlane/plugin/mango/helper/emulator_language.rb
+++ lib/fastlane/plugin/mango/helper/emulator_language.rb
@@ -11,15 +11,15 @@ module Fastlane
           UI.important("Changing device locale to #{lang}")
           api = Actions.sh('adb shell getprop ro.build.version.sdk').to_i
           if api < 28
-            Aruba::ADB.execute("adb shell am broadcast -a com.android.intent.action.SET_LOCALE --es com.android.intent.extra.LOCALE \"#{lang}\" com.android.customlocale2")
+            Actions.sh("adb shell am broadcast -a com.android.intent.action.SET_LOCALE --es com.android.intent.extra.LOCALE \"#{lang}\" com.android.customlocale2")
           else
             # On API levels higher than 27 we need to use the appium settings app to set system settings like the locale
-            Kernel.puts('Using Appium Settings to set the device locale!')
+            UI.importan('Using Appium Settings to set the device locale!')
             language = lang.split('_')[0]
             country = lang.split('_')[1]
-            Aruba::ADB.execute('install ../settings_apk/settings_apk-debug.apk')
-            Aruba::ADB.execute('shell pm grant io.appium.settings android.permission.CHANGE_CONFIGURATION')
-            Aruba::ADB.execute("shell am broadcast -a io.appium.settings.locale -n io.appium.settings/.receivers.LocaleSettingReceiver --es lang #{language} --es country #{country}")
+            Actions.sh('adb install ../settings_apk/settings_apk-debug.apk')
+            Actions.sh('adb shell pm grant io.appium.settings android.permission.CHANGE_CONFIGURATION')
+            Actions.sh("adb shell am broadcast -a io.appium.settings.locale -n io.appium.settings/.receivers.LocaleSettingReceiver --es lang #{language} --es country #{country}")
           end
         end
       end
